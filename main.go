f4b9511c946b3b0b7372bdee00163291e3736af6
Luc Chmielowski <luc.chmielowski@gmail.com>
2017-06-05 11:58:36 +0200

    Added Credentials model

---
+++ main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/iochti/auth-service/models"
+    "golang.org/x/oauth2"
+    "golang.org/x/oauth2/github"
+)
+
+var conf *oauth2.Config
+
+// Inits the service state & conf
+func init() {
+
+}
+
+func main() {
+}
+
+func
